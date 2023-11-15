64ce81d2ec324a077d8bcab556e547e252de449d
Isaac Arroyo <isaacarroyov@outlook.com>
2023-11-14 23:52:03 -0600

    Update 2023_30daymapchallenge_day11_retro.R
    Mapa casi listo.
    
    TODO: fonts detalles de composicion

--- scripts/2023_30daymapchallenge_day11_retro.R
+++ scripts/2023_30daymapchallenge_day11_retro.R
@@ -5,27 +5,56 @@ library(sysfonts)
 library(showtextdb)
 library(showtext)
 library(sf)
+library(geojsonsf)
 sf_use_s2(FALSE)
 
 # = = LOAD DATA = = #
-uso_suelo_s1 <- st_read("path/to/usv_serie1_gcs_JydS1Ls.shp") # nolint
+# - - Uso de Suego y Vegetación Serie I de Yucatán (1978 - 1991)
+sf_usv_s01_yuc <- geojson_sf("./data/usv_series_01_yucatan.geojson") %>%
+janitor::clean_names()
 
-sf_ent_yuc <- geojsonsf::geojson_sf("https://raw.githubusercontent.com/isaacarroyov/datos_facil_acceso/main/Gobierno-Mexicano/geometrias/00ent_mexico.geojson") %>% filter(cve_geo == "31") # nolint
+# - - Geometrías del estado de Yucatán de acuerdo al Marco Geostadístico de México (2022) - - # nolint
+sf_ent_yuc <- st_read("https://raw.githubusercontent.com/isaacarroyov/datos_facil_acceso/main/Gobierno-Mexicano/geometrias/00ent_mexico.geojson") %>% # nolint
+  janitor::clean_names() %>%
+  filter(cve_geo == "31")
 
-# = = DATA PROCESSING 00 = = #
-# - - Dar ID a los elementos del sfc - - #
-uso_suelo_s1 <- mutate(uso_suelo_s1, id_row = 1:nrow(uso_suelo_s1))
+# = = DATA PROCESSING = = #
+# - - Agrupar `clase` en categorias más reducidas - - #
+sf_usv_s01_yuc_datavis <- sf_usv_s01_yuc %>%
+  mutate(clase_reduced = case_when(
+    str_detect(clase, "AGRICULTURA") ~ "Agricultura",
+    clase == "AREA SIN VEGETACION APARENTE" ~ "Sin vegetación aparente",
+    clase == "OTRO TIPO DE VEGETACION" ~ "Otro tipo de vegetación",
+    str_detect(clase, "PASTIZAL") ~ "Pastizal",
+    str_detect(clase, "SELVA") ~ "Selva",
+    str_detect(clase, "HIDROFILA") ~ "Vegetación hidrófila",
+    .default = str_to_sentence(clase)))
 
-# - - Crear copia de centroides - - #
-uso_suelo_s1_centroids <- st_centroid(uso_suelo_s1, of_largest_polygon = TRUE)
+# = = DATAVIS = = #
+mapa_usv_s01_yuc <- sf_usv_s01_yuc_datavis %>%
+  ggplot(aes(fill = clase_reduced)) +
+  geom_sf(linewidth = 0.1, color = "black") +
+  geom_sf(data = sf_ent_yuc,
+          fill = "transparent",
+          color = "#E4E5E2",
+          linewidth = 0.5) +
+  scale_fill_manual(values = moma.colors("Ohchi", n = 9)) +
+  labs(title = "#30DayMapChallenge Day 11: Retro",
+       subtitle = "Uso de Suelos y Vegetación en Yucatán, Serie I (1978-1991)",
+       caption = "<b>Datos</b>:<em>Uso del Suelo y Vegetación, (Serie I), en México, INEGI, 1978-1991</em> via <b>IDEGEO</b>") + # nolint
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#171D0C"),
 
-# - - Encontrar los centroides que se encuentran dentro de Yucatán - - #
-uso_suelo_s1_centroids_in_yuc <- st_filter(x = uso_suelo_s1_centroids, y = sf_ent_yuc, .predicate = st_within) # nolint
-vec_ids_uso_suelo_s1_in_yuc <- uso_suelo_s1_centroids_in_yuc %>% as_tibble() %>% pull(id_row) # nolint
+    legend.position = "bottom",
+    legend.title = element_text(color = "#EBE7F2", face = "bold"),
+    legend.text = element_text(color = "#E4E5E2"),
 
-# - - Filtrar a los centroides "yucatecos" y guardar como GeoJSON - - #
-uso_suelo_s1_in_yuc <- filter(.data = uso_suelo_s1, id_row %in% vec_ids_uso_suelo_s1_in_yuc) # nolint
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.title = element_textbox(color = "#E4E5E2"),
+    plot.subtitle = element_textbox(color = "#E4E5E2"),
+    plot.caption = element_textbox(color = "#E4E5E2", hjust = 0)
+  )
 
-uso_suelo_s1_in_yuc %>%
-  select(-id_row) %>%
-  st_write("./data/usv_series_01_yucatan.geojson", driver = "GeoJSON")
+tgutil::ggpreview(mapa_usv_s01_yuc, width = 11, height = 10, dpi = 300)
