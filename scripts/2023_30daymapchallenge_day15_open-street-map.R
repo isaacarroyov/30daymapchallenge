a0f8c75d96186f316041039d8cb4b066f3c85596
Isaac Arroyo <isaacarroyov@outlook.com>
2023-11-16 10:09:18 -0600

    Update Day 15: OSM
    - Se escribió mal university y se corrigio
    - Se agregaron areas naturales verdes, es decir: Monte
    - Se cambia de color landuse green, solo se usa para resaltar ciertos espacios
    - Se cambia el orden de las capas para que se puedan ver las calles

--- scripts/2023_30daymapchallenge_day15_open-street-map.R
+++ scripts/2023_30daymapchallenge_day15_open-street-map.R
@@ -11,15 +11,16 @@ library(osmdata)
 # ~ ~ Bouding Box ~ ~ #
 chetumal_bb <- getbb("Chetumal, Quintana Roo, Mexico")
 
-# ~ ~ Landuse: cualquier cosa que sea green y algo recreativo ~ ~ #
+# ~ ~ Parques ~ ~ #
 chetumal_parks <- chetumal_bb %>%
   opq() %>%
   add_osm_feature(key = "leisure", value = c("park", "playground", "garden", "track")) %>% # nolint
   osmdata_sf()
 
-chetumal_landuse_green_areas <- chetumal_bb %>%
+# ~ ~ Natural: Green areas ~ ~ #
+chetumal_natural_green_areas <- chetumal_bb %>%
   opq() %>%
-  add_osm_feature(key = "landuse", value = c("greenfield", "recreation_ground", "grass", "village_green")) %>% # nolint
+  add_osm_feature(key = "natural", value = c("grassland", "heath", "scrub", "shrubbery", "wood")) %>% # nolint
   osmdata_sf()
 
 chetumal_agua <- chetumal_bb %>%
@@ -30,7 +31,7 @@ chetumal_agua <- chetumal_bb %>%
 # ~ ~ educación: escuelas y universidades ~ ~ #
 chetumal_schools <- chetumal_bb %>%
   opq() %>%
-  add_osm_feature(key = "amenity", value = c("school", "unversity")) %>%
+  add_osm_feature(key = "amenity", value = c("school", "university")) %>%
   osmdata_sf()
 
 # ~ ~ highway: street data ~ ~ #
@@ -60,6 +61,10 @@ showtext_auto()
 
 # - - Map - - #
 mapa_chetumal <- ggplot() +
+  # ~ ~ Areas verdes ~ ~ #
+  geom_sf(data = chetumal_natural_green_areas$osm_polygons,
+          fill = "#556B2F",
+          color = "black", linewidth = 0) +
   # ~ ~ Calles ~ ~ #
   geom_sf(data = cheutmal_big_streets$osm_lines,
           color = "black", linewidth = 0.5) +
@@ -71,7 +76,8 @@ mapa_chetumal <- ggplot() +
   geom_sf(data = chetumal_agua$osm_lines,
           color = "#4169E1") +
   geom_sf(data = chetumal_agua$osm_polygons,
-          fill = "#4169E1", color = "#4169E1") +
+          fill = "#4169E1",
+          color = "#000000", linewidth = 0.2) +
   # ~ ~ Escuelas ~ ~ #
   geom_sf(data = chetumal_schools$osm_polygons,
           fill = "#DAA520",
@@ -80,13 +86,9 @@ mapa_chetumal <- ggplot() +
   geom_sf(data = chetumal_parks$osm_polygons,
           fill = "#8B4513",
           color = "black", linewidth = 0) +
-  # ~ ~ Areas verdes ~ ~ #
-  geom_sf(data = chetumal_landuse_green_areas$osm_polygons,
-          fill = "#556B2F",
-          color = "black", linewidth = 0) +
   # ~ ~ Title + Annotations ~ ~ #
   labs(title = "#30DayMapChallenge Day 15: OpenStreetMap<br> Lugares de Chetumal, Quintana Roo", # nolint
-       subtitle = "<b style='color:#000000;'>Calles</b>, <b style='color:#556B2F;'>Areas Verdes</b>, <b style='color:#8B4513;'>Parques</b>, <b style='color:#C3941D;'>Escuelas</b> y <b style='color:#2B58DE;'>Cuerpos de agua</b><br><em style='font-size:30px;'><b>Nota</b>: En los <b style='color:#2B58DE;'>cuerpos de agua</b> se incluye la costa, bordes de cuerpos agua o elementos donde hay presencia significativa de agua)</em>", # nolint
+       subtitle = "<b style='color:#000000;'>Calles</b>, <b style='color:#556B2F;'>Monte</b>, <b style='color:#8B4513;'>Parques</b>, <b style='color:#C3941D;'>Escuelas</b> y <b style='color:#2B58DE;'>Cuerpos de agua</b><br><em style='font-size:30px;'><b>Nota</b>: En los <b style='color:#2B58DE;'>cuerpos de agua</b> se incluye la costa, bordes de cuerpos agua o elementos donde hay presencia significativa de agua)</em>", # nolint
        caption = "<em>Isaac Arroyo (@unisaacarroyov)<br><b>Data</b>: Map data © OpenStreetMap contributors</em>") + # nolint
   theme_void() +
   theme(
@@ -109,7 +111,7 @@ mapa_chetumal <- ggplot() +
       halign = 0.5,
       hjust = 0.5,
       lineheight = 0.2,
-      margin = margin(b = -1, t = 0.2, unit = "in"), # nolint
+      margin = margin(b = -0.2, t = 0.2, unit = "in"), # nolint
       family = "body",
       width = unit(7, "in")),
     plot.caption = element_textbox(
